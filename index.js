b99906251dced97b6011b57cb1db969dd0644d79
DavideFolk <107867374+DavideFolk@users.noreply.github.com>
2022-11-25 10:34:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ let accountData = {
 		password: '123',
 		email: 'davide@prova.it',
 		piano: 'Premium',
-		idConto: 'dapr001',
-		importiConto: [1000, -100, 450, -20, -30, 1200, -350],
+		idConto: 'davpro001',
+		importiConto: [
+			{ importo: 1000, notaSpesa: 'Stipendio - arch invest' },
+			{ importo: -100, notaSpesa: 'Zara' },
+			{ importo: -450, notaSpesa: 'Bonifico: Affitto' },
+			{ importo: -20, notaSpesa: 'Pizzeria Napoli srl' },
+			{ importo: -30, notaSpesa: 'Gamestop Varese' },
+			{ importo: 1200, notaSpesa: 'Bonifico' },
+			{ importo: -350, notaSpesa: 'Bonifico' },
+		],
 		investimenti: {
 			azioni: [
 				['FCA', 1000],
@@ -55,32 +63,61 @@ function logIn(user) {
 function renderHome() {
 	$('#titleName').text(`Ciao, ${accountData[currentUser].nome}.`);
 
-	let totaleConto = accountData[currentUser].importiConto.reduce(
-		(prev, curr) => prev + curr
-	);
+	let totaleConto = 0.0;
+	if (accountData[currentUser].importiConto[0]) {
+		for (const i in accountData[currentUser].importiConto) {
+			let notaSpesa = accountData[currentUser].importiConto[i].notaSpesa;
+			let importoAttuale =
+				accountData[currentUser].importiConto[i].importo;
+			totaleConto += importoAttuale;
+
+			$('.list-group')
+				.append(`<div class="list-group-item text-bg-dark d-flex gap-3 py-3 shadow" aria-current="true">
+			${
+				importoAttuale > 0
+					? '<i class="bi bi-plus"></i>'
+					: '<i class="bi bi-dash"></i>'
+			}
+			<div class="d-flex gap-2 w-100 justify-content-between">
+				<div>
+					<h4 class="mb-0">${Math.abs(importoAttuale).toLocaleString('it-IT')} €</h4>
+					<p class="mb-0 opacity-75">${notaSpesa}</p>
+				</div>
+			</div>
+		</div>`);
+		}
+	}
 	$('#totaleConto').text(
 		`${totaleConto.toLocaleString('it-IT', { minimumFractionDigits: 2 })} €`
 	);
 
 	let totaleInv = 0.0;
-	for (const i in accountData[currentUser].investimenti) {
-		accountData[currentUser].investimenti[i].forEach((element) => {
-			totaleInv += element[1];
-		});
+	if (accountData[currentUser].investimenti) {
+		for (const i in accountData[currentUser].investimenti) {
+			accountData[currentUser].investimenti[i].forEach((element) => {
+				totaleInv += element[1];
+			});
+		}
 	}
 	$('#totaleInv').text(
 		`${totaleInv.toLocaleString('it-IT', { minimumFractionDigits: 2 })} €`
 	);
 
 	let totaleCrypto = 0.0;
-	accountData[currentUser].crypto.forEach((element) => {
-		totaleCrypto += element[1];
-	});
+	if (accountData[currentUser].crypto) {
+		accountData[currentUser].crypto.forEach((element) => {
+			totaleCrypto += element[1];
+		});
+	}
 	$('#totaleCrypto').text(
 		`${totaleCrypto.toLocaleString('it-IT', {
 			minimumFractionDigits: 2,
 		})} €`
 	);
+
+	$('#idConto').text(accountData[currentUser].idConto);
+
+	$('#tipoConto').text(accountData[currentUser].piano);
 }
 
 function iscrizione() {
@@ -90,16 +127,24 @@ function iscrizione() {
 	let username = $('#inputUserName').val();
 	let password = $('#inputPassword').val();
 	let piano = $('#inputPiano').val();
+	let lunghezza = Object.keys(accountData).length + 1;
+	let idConto =
+		nome.slice(0, 3).toLowerCase() +
+		cognome.slice(0, 3).toLowerCase() +
+		lunghezza.toString().padStart(3, '0');
 	accountData[username] = {
 		nome: nome,
 		cognome: cognome,
 		password: password,
 		email: email,
 		piano: piano,
+		idConto: idConto,
+		importiConto: [0.0],
 	};
 	account.push(username);
 	$('#iscrModal').modal('toggle');
 	$('#modalOk').modal('toggle');
+	console.log(accountData);
 }
 
 //Onclick modal login
